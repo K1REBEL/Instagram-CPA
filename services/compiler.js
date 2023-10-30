3e3fd19ad994317ff16a90b832ffeee26c39b875
K1REBEL <kibraheem47@yahoo.com>
2023-10-30 20:13:10 +0300

    Can now generate Excel files!

--- services/compiler.js
+++ services/compiler.js
@@ -1,19 +1,22 @@
 const sqlite3 = require('sqlite3').verbose();
 const XLSX = require('xlsx');
-const { getDatabase } = require("../DB/sqliteconnection");
+const path = require ("path");
+const { sqlient, getDatabase } = require("../DB/sqliteconnection");
 
 const today = new Date();
 const date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 
 // Open the database connection
+sqlient();
 const db = getDatabase();
 
 // Query the table and extract the data
-db.all(`SELECT * FROM requests WHERE date = ${date}`, [], (err, rows) => {
+db.all(`SELECT * FROM requests WHERE date = '${date}'`, [], (err, rows) => {
   if (err) {
     throw err;
   }
 
+  // console.log(rows);
   // Convert the data to a worksheet
   let worksheet = XLSX.utils.json_to_sheet(rows);
 
@@ -22,8 +25,6 @@ db.all(`SELECT * FROM requests WHERE date = ${date}`, [], (err, rows) => {
   XLSX.utils.book_append_sheet(workbook, worksheet, today.getDate());
 
   // Write the workbook to a file
-  XLSX.writeFile(workbook, 'output.xlsx');
+  XLSX.writeFile(workbook, path.join(__dirname, `../excel/${(today.getMonth()+1)}.xlsx`));
 });
 
-// Close the database connection
-db.close();
